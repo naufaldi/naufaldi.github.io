fbdde2bc4e4c5474b6548de981ee25e9657daafd
naufaldi <naufaldi.rafif@gmail.com>
2018-11-14 19:32:59 +0700

    setting card name

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Grid, Cell} from 'react-mdl';
 import Cardlanding from './card-landing'
+import CardName from './cardName';
 
 class Landingpage extends Component {
   render () {
@@ -8,7 +9,7 @@ class Landingpage extends Component {
       <div style={{width: '100%', margin:'auto'}} >
       <Grid className="landing-grid" >
         <Cell col={12} className="flex flex-align">
-         <Cardlanding />
+         <CardName />
         </Cell>
       </Grid>
       </div>
