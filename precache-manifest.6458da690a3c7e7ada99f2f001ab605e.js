2ce488f8078b024a39d31efff4a960e1b193bef9
naufaldi <naufaldi.rafif@gmail.com>
2019-08-15 09:27:07 +0700

    Updates

---
+++ precache-manifest.6458da690a3c7e7ada99f2f001ab605e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8b7cc2047b65aae3b57",
+    "url": "/static/css/main.fafb52b1.chunk.css"
+  },
+  {
+    "revision": "a8b7cc2047b65aae3b57",
+    "url": "/static/js/main.a8b7cc20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "cf705d5c549faf6383b0",
+    "url": "/static/js/2.cf705d5c.chunk.js"
+  },
+  {
+    "revision": "ad2d864da1624485dffd9b14bed55b6c",
+    "url": "/static/media/design.ad2d864d.jpg"
+  },
+  {
+    "revision": "62c71efa1330e442b4213c2042088059",
+    "url": "/static/media/naufaldi.62c71efa.jpg"
+  },
+  {
+    "revision": "141a737df0de97ac414fd960a7003785",
+    "url": "/static/media/cat-1.141a737d.jpeg"
+  },
+  {
+    "revision": "54a7201b1710524d72a7a914072f4911",
+    "url": "/index.html"
+  }
+];
